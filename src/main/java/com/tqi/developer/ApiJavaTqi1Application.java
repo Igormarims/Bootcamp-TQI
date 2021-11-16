ec7e4f38f96ba89a83d55b2e847211aef9ff522b
Igor marques <igormarquespe@gmail.com>
2021-11-16 08:26:29 -0300

    creating project and connecting with bank

---
+++ src/main/java/com/tqi/developer/ApiJavaTqi1Application.java
@@ -0,0 +1,13 @@
+package com.tqi.developer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJavaTqi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJavaTqi1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/tqi/developer/ApiJavaTqi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tqi.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJavaTqi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
